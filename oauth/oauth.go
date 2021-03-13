bdf4aa3955846f9943b4c4246c47c0ea0abc04a9
twinemarron <twinemarron@gmail.com>
2021-03-13 16:26:54 +0900

    Add logger

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -116,6 +116,8 @@ func getAccessToken(accessTokenId string) (*accessToken, *errors.RestErr) {
 
 	var at accessToken
 	if err := json.Unmarshal(response.Bytes(), &at); err != nil {
+		fmt.Println("log...Unmarshal...err...")
+		fmt.Println(err)
 		return nil, errors.NewInternalServerError("error when trying unmarshall access token response")
 	}
 	return &at, nil
